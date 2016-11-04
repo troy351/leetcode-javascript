ead1add9e86aeba8b618339aa5a43795c234d13e
troy <914053923@qq.com>
2016-11-04 11:18:45 +0800

    improved algorithm

--- 01~99/18. 4Sum.js
+++ 01~99/18. 4Sum.js
@@ -7,40 +7,33 @@ var fourSum = function (nums, target) {
     nums.sort(function (a, b) {
         return a - b;
     });
-    var map = new Map();
     var result = [];
-    var current = Number.MAX_VALUE;
 
-    for (var i = 0; i < nums.length; i++) {
-        if (current === nums[i]) continue;
-        var tNums = nums.slice();
-        current = nums[i];
-        tNums.splice(i, 1);
-        threeSum(tNums, target - current);
+    var i = 0;
+    while (i <= nums.length - 4) {
+        threeSum(i, nums, target - nums[i]);
+        while (nums[i] === nums[++i] && i <= nums.length - 4);
     }
 
     return result;
 
     // used code from question 15, made some small changes
-    function threeSum(nums, target) {
-        var i = 0;
+    function threeSum(start, nums, target) {
+        var i = start + 1;
         while (i < nums.length - 2) {
             var j = i + 1;
             var k = nums.length - 1;
             while (j < k) {
                 var sum = nums[i] + nums[j] + nums[k];
                 if (sum === target) {
-                    var key = [nums[i], nums[j], nums[k], current].sort();
-                    if (!map.get(key.toString())) {
-                        // result.push(key);
-                        map.set(key.toString(), 1);
-                    }
-                    result.push(key);
+                    result.push([nums[start], nums[i], nums[j], nums[k]]);
                 }
-                if (sum <= target) while (nums[j] == nums[++j] && j < k);
-                if (sum >= target) while (nums[k--] == nums[k] && j < k);
+                if (sum <= target) while (nums[j] === nums[++j] && j < k);
+                if (sum >= target) while (nums[k--] === nums[k] && j < k);
             }
-            while (nums[i] == nums[++i] && i < nums.length - 2);
+            while (nums[i] === nums[++i] && i < nums.length - 2);
         }
     }
 };
+
+console.log(fourSum([1, 0, -1, 0, -2, 2], 0))
