3e80b986ef4cb7711f630a1538c26f5d7d714021
troy <914053923@qq.com>
2016-11-25 20:18:24 +0800

    reformat code

--- 01~99/08. String to Integer (atoi).js
+++ 01~99/08. String to Integer (atoi).js
@@ -5,16 +5,14 @@
 var myAtoi = function (str) {
     str = str.trim();
     let i = str[0] === '-' || str[0] === '+' ? 1 : 0;
-    const sign = str[0] === '-' ? -1 : 1;
+    const sign = str[0] === '-' ? -1 : 1, MAX_VALUE = Math.pow(2, 31) - 1;
     let result = 0;
-    const MAX_VALUE = Math.pow(2, 31) - 1;
 
     while (true) {
-        const ascii = str.charCodeAt(i);
+        const ascii = str.charCodeAt(i++);
         if (isNaN(ascii)) break;
         if (ascii < 58 && ascii >= 48) {
             result = result * 10 + ascii - 48;
-            i++;
             // if out of 32-bit integer range, return the maximum or minimum number
             if (sign * result > MAX_VALUE) return MAX_VALUE;
             else if (sign * result < -(MAX_VALUE + 1)) return -(MAX_VALUE + 1);
